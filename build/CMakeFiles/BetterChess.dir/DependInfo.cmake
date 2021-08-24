f9c62c4a8e6bbbbaf9a7fae740b67571c7cd6eb3
Bor Brudar <bor.brudar@gmail.com>
2021-08-24 22:46:27 +0200

    final?

---
+++ build/CMakeFiles/BetterChess.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bor/source/BetterChess/src/Bishop.cpp" "/home/bor/source/BetterChess/build/CMakeFiles/BetterChess.dir/src/Bishop.cpp.o"
+  "/home/bor/source/BetterChess/src/Board.cpp" "/home/bor/source/BetterChess/build/CMakeFiles/BetterChess.dir/src/Board.cpp.o"
+  "/home/bor/source/BetterChess/src/BoardV.cpp" "/home/bor/source/BetterChess/build/CMakeFiles/BetterChess.dir/src/BoardV.cpp.o"
+  "/home/bor/source/BetterChess/src/Chessboard.cpp" "/home/bor/source/BetterChess/build/CMakeFiles/BetterChess.dir/src/Chessboard.cpp.o"
+  "/home/bor/source/BetterChess/src/Game.cpp" "/home/bor/source/BetterChess/build/CMakeFiles/BetterChess.dir/src/Game.cpp.o"
+  "/home/bor/source/BetterChess/src/King.cpp" "/home/bor/source/BetterChess/build/CMakeFiles/BetterChess.dir/src/King.cpp.o"
+  "/home/bor/source/BetterChess/src/Knight.cpp" "/home/bor/source/BetterChess/build/CMakeFiles/BetterChess.dir/src/Knight.cpp.o"
+  "/home/bor/source/BetterChess/src/Main.cpp" "/home/bor/source/BetterChess/build/CMakeFiles/BetterChess.dir/src/Main.cpp.o"
+  "/home/bor/source/BetterChess/src/Pawn.cpp" "/home/bor/source/BetterChess/build/CMakeFiles/BetterChess.dir/src/Pawn.cpp.o"
+  "/home/bor/source/BetterChess/src/Piece.cpp" "/home/bor/source/BetterChess/build/CMakeFiles/BetterChess.dir/src/Piece.cpp.o"
+  "/home/bor/source/BetterChess/src/PromotionMenu.cpp" "/home/bor/source/BetterChess/build/CMakeFiles/BetterChess.dir/src/PromotionMenu.cpp.o"
+  "/home/bor/source/BetterChess/src/Queen.cpp" "/home/bor/source/BetterChess/build/CMakeFiles/BetterChess.dir/src/Queen.cpp.o"
+  "/home/bor/source/BetterChess/src/Rook.cpp" "/home/bor/source/BetterChess/build/CMakeFiles/BetterChess.dir/src/Rook.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../res"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
